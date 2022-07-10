7ba748f59c9ae79e244f86c71d4f832667e19933
Taha Yaminy <tahaYaminy4@gmail.com>
2022-07-10 19:57:47 +0430

    finish design

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'black':'#222831',
+      'gray':'#393E46',
+      'blue':'#00ADB5',
+      'white':{
+        '0':'#f0f0f0',
+        '1':'#eeeeee'
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
